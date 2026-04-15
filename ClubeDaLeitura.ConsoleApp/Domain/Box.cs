84c0ac9b61233f9df75d540c7ca77f3b6bd20f22
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-04-15 15:54:29 -0300

    adicionada classe de domínio ''Box'' com propriedades + constructor

--- ClubeDaLeitura.ConsoleApp/Domain/Box.cs
+++ ClubeDaLeitura.ConsoleApp/Domain/Box.cs
@@ -1,6 +1,22 @@
+using System.Security.Cryptography;
 namespace ClubeDaLeitura.ConsoleApp.Domain;
 
 public class Box
 {
+  public string Id { get; set; } = string.Empty;
+  public string Label { get; set; } = string.Empty;
+  public string Color { get; set; } = string.Empty;
+  public int LoanDays { get; set; } = 7;
 
+  public Box(string label, string color, int loanDays)
+  {
+    Id = Convert
+            .ToHexString(RandomNumberGenerator.GetBytes(20))
+            .ToLower()
+            .Substring(0, 7);
+
+    Label = label;
+    Color = color;
+    LoanDays = loanDays;
+  }
 }
