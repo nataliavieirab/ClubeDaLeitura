a3ecc135ee0d18ede7469ffc97eea7ff4c7805f8
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-04-21 16:45:15 -0300

    feat: add friend management features to Program.cs

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -4,15 +4,20 @@ using ClubeDaLeitura.ConsoleApp.Presentation;
 
 BoxRepository boxRepository = new();
 MagazineRepository magazineRepository = new();
+FriendRepository friendRepository = new();
 
 BoxScreen boxScreen = new(boxRepository);
 MagazineScreen magazineScreen = new(magazineRepository, boxRepository, boxScreen);
+FriendScreen friendScreen = new(friendRepository);
 
 Box box = new("Lançamentos", "Vermelho", 3);
 boxRepository.Create(box);
 
 Magazine magazine = new("Action Comics", 155, 1990, box);
 magazineRepository.Create(magazine);
+
+Friend friend = new("Savarininho", "Savarino", "48 99976 5544");
+friendRepository.Create(friend);
 
 ScreenUtils screen = new("Clube da Leitura");
 
@@ -30,7 +35,7 @@ while (true)
   {
     string? innerMenuOption = string.Empty;
 
-    if (mainMenuOption == "1")
+    if (mainMenuOption == "1") // Boxes
     {
       innerMenuOption = boxScreen.GetMenuOption();
 
@@ -49,7 +54,7 @@ while (true)
       else if (innerMenuOption == "4") boxScreen.ShowAll(showHeader: true);
     }
 
-    else if (mainMenuOption == "2")
+    else if (mainMenuOption == "2") // Magazines
     {
       innerMenuOption = magazineScreen.GetMenuOption();
 
@@ -68,9 +73,23 @@ while (true)
       else if (innerMenuOption == "4") magazineScreen.ShowAll(showHeader: true);
     }
 
-    else if (mainMenuOption == "3")
+    else if (mainMenuOption == "3") // Friends
     {
+      innerMenuOption = friendScreen.GetMenuOption();
 
+      if (innerMenuOption == "S")
+      {
+        Console.Clear();
+        break;
+      }
+
+      if (innerMenuOption == "1") friendScreen.Register();
+
+      else if (innerMenuOption == "2") friendScreen.Edit();
+
+      else if (innerMenuOption == "3") friendScreen.Delete();
+
+      else if (innerMenuOption == "4") friendScreen.ShowAll(showHeader: true);
     }
 
     else if (mainMenuOption == "4")
